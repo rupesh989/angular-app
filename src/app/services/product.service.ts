57af17720f062d5cc748dbd5ca21a2e6ca8c3c6a
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-21 22:30:34 +0530

    changes for app

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
-import { animate } from '@angular/animations';
-import { Paginationparams, Product, Products } from '../../../types';
+import { PaginationParams, Products } from '../../types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private apiService: ApiService) { }
 
-  constructor(private apiservice: ApiService) { }
-
-  getProducts = (url: string, params: Paginationparams): Observable<Products> => {
-    return this.apiservice.get(url, {
-      params, responseType: 'json',
+  // Getting products from the API
+  getProducts = (
+    url: string,
+    params: PaginationParams
+  ): Observable<Products> => {
+    return this.apiService.get(url, {
+      params,
+      responseType: 'json',
     });
-  }
+  };
+
+  // Adding a product via the API
+  addProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.post(url, body, {});
+  };
+
+  // Editing a product via the API
+  editProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.put(url, body, {});
+  };
+
+  // Deleting a product via the API
+  deleteProduct = (url: string): Observable<any> => {
+    return this.apiService.delete(url, {});
+  };
 }
