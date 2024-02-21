4f1728be31471145720c499924dbcf754ad08b94
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-21 22:29:01 +0530

    changes for app

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { options } from '../../../types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+  get<T>(url: string, options: options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
 }
+
