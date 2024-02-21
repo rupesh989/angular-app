384480a74eca7ce79c167b7ef072a079e5e40d32
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-21 22:27:56 +0530

    changes for app

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { animate } from '@angular/animations';
+import { Paginationparams, Product, Products } from '../../../types';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductsService {
 
-  constructor() { }
+  constructor(private apiservice: ApiService) { }
+
+  getProducts = (url: string, params: Paginationparams): Observable<Products> => {
+    return this.apiservice.get(url, {
+      params, responseType: 'json',
+    });
+  }
 }
