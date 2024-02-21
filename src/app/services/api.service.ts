2e5a140377294d9e13bdd25d92f72b918a320d90
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-21 22:31:54 +0530

    changes for app

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { options } from '../../../types';
+import { Options, Product } from '../../types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) { }
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
-  get<T>(url: string, options: options): Observable<T> {
+  // Used to make a GET request to the API
+  get<T>(url: string, options: Options): Observable<T> {
     return this.httpClient.get<T>(url, options) as Observable<T>;
   }
-}
 
+  // Used to make a POST request to the API
+  post<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.post<T>(url, body, options) as Observable<T>;
+  }
+
+  // Used to make a PUT request to the API
+  put<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.put<T>(url, body, options) as Observable<T>;
+  }
+
+  // Used to make a DELETE request to the API
+  delete<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.delete<T>(url, options) as Observable<T>;
+  }
+}
