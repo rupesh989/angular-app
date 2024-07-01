38a99c3a7f6ac8f6bdbeccfe81a50a03a9ad74b1
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-07-01 23:42:27 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,1 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from "./layout/header/header.component";
-import { FooterComponent } from "./layout/footer/footer.component";
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent]
-})
-export class AppComponent {
-  title = 'practiceApp';
-}
