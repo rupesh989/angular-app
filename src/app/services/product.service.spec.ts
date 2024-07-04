8ea93e73431fbb3bec7ae0eaf58036d596e3d20e
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-07-04 21:50:56 +0530

    Update product.service.spec.ts

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { ProductService } from './product.service';
-
-describe('ProductService', () => {
-  let service: ProductService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
