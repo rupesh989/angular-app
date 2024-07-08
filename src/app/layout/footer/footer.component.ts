11e2bad91fc2c892b886fe47aada9ac61f1ed47d
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-07-08 22:54:59 +0530

    Update footer.component.ts

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
-})
-export class FooterComponent {
-
-}
